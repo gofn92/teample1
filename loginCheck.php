5a06ac99712f883474971d4a1e87c4137a9bd295
dysdyd <78690021+dysdyd@users.noreply.github.com>
2021-02-23 13:53:46 +0900

    로그인상태확인

--- loginCheck.php
+++ loginCheck.php
@@ -6,19 +6,19 @@
 		<title> loginCheck.php  </title>
 	</head>
 	<body>
-		<?php
+	</body>
+	<?php
         // $prevpage = $_SERVER['HTTP_REFERER'];
 		$u_id = $_REQUEST["u_id"];
 		$u_pw = $_REQUEST["u_pw"];
-		$logchk = $_REQUEST["logchk"];
-		
+
 		$conn = mysqli_connect("localhost","dysdyd","ckddud!159","dysdyd");
 		if(!$conn){
 				die("접속이 되지 않았습니다.".mysqli_connect_error());
 		}
-		
+
 		$str = "SELECT * FROM signUp WHERE u_id='$u_id'";
-		$result = mysqli_query($conn,$str);	
+		$result = mysqli_query($conn,$str);
 
 		if(!$result){
 			echo"등록되지않은아이디입니다.".mysqli_error($conn);
@@ -32,16 +32,16 @@
 		if($u_pw != $db_pass){
 			echo("<script>
 				alert('비번이 틀립니다'.);
-				document.write('$db_pass');
-				document.write('$u_pw');
+				document.location.href='signIn.php';
 			</script>");
-			exit;
+			// exit;
 		}
 		else{
+			setcookie("loginCook",$u_id,time()+3600);
 			echo("<script>
 				alert('로그인에 성공했습니다!');
-			</script");
+				document.location.href='index.php';
+			</script>");
 		}
-		?>
-	</body>
+	?>
 </html>
